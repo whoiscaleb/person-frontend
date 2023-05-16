8b16f67e3a59555f1961ed46f76172bf65de4b49
whoiscaleb <caleb23227@gmail.com>
2023-05-16 11:02:22 -0400

    removed useParams from Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import { Routes, Route, useParams } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 
 import Index from '../pages/Index'
 import Show from '../pages/Show'
